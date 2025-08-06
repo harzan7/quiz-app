8187ccd78a2150a2beb81bb3639093d94ca580a8
Harza <harzan12345@gmail.com>
2025-08-06 07:36:02 -0400

    Initial UI layout for quiz app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./index.css"
+import Quiz from './components/Quiz'
 
 function App() {
   return (
-    <> Hello World! </>
+    <div className="app-container">
+      <h1>Quiz App</h1>
+      <Quiz />
+    </div>
   )
 }
 
