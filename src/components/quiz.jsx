baf9d8383e57499614abb528b9a6f9f91a225578
Harza <harzan12345@gmail.com>
2025-08-06 06:50:58 -0400

    Add question bank of objects (question, options, answer) as constant to quiz.jsx.

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -1,5 +1,27 @@
 
 function Quiz() {
+    const questionBank = [
+        {
+            question: "What is the capital of France?",
+            options: ["Berlin", "London", "Paris", "Rome"],
+            answer: "Paris",
+        },
+        {
+            question: "Which language is used for web apps?",
+            options: ["PHP", "Python", "Javascript", "All"],
+            answer: "All",
+        },
+        {
+            question: "What does JSX stand for?",
+            options: [
+                "JavaScript XML",
+                "Java Syntax eXtension",
+                "Just a Simple eXample",
+                "None of the above",
+            ],
+            answer: "JavaScript XML",
+        },
+    ]
     return <div>Quiz</div>
 }
 
