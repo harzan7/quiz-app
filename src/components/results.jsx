62653b6e38decace4deb35d728d7e9d532da09e8
Harza <harzan12345@gmail.com>
2025-08-11 16:09:17 -0400

    Add basic layout for results page.

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,5 +1,11 @@
 function Results() {
-    return <div></div>
+    return (
+        <div>
+            <h2>Quiz Completed!</h2>
+            <p>Your Score: 2/3</p>
+            <button className={'restart-button'}>Restart Quiz</button>
+        </div>
+    );
 }
 
 export default Results;
